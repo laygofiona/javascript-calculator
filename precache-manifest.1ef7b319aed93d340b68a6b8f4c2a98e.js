b34dfc6568f24ab6a50ede00a28093f723449ae9
laygofiona <laygo.fiona@gmail.com>
2023-05-01 16:48:41 +0900

    Updates

---
+++ precache-manifest.1ef7b319aed93d340b68a6b8f4c2a98e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1e1ee29506742ef30daf",
+    "url": "/javascript-calculator/static/css/main.d718d6e1.chunk.css"
+  },
+  {
+    "revision": "1e1ee29506742ef30daf",
+    "url": "/javascript-calculator/static/js/main.1e1ee295.chunk.js"
+  },
+  {
+    "revision": "d4956f17f91e22a24c4b",
+    "url": "/javascript-calculator/static/js/1.d4956f17.chunk.js"
+  },
+  {
+    "revision": "9bc7773250f7c400c052",
+    "url": "/javascript-calculator/static/js/runtime~main.9bc77732.js"
+  },
+  {
+    "revision": "c5a32cc1c61109d3060b82a05ebb9433",
+    "url": "/javascript-calculator/index.html"
+  }
+];
